48ffd31d5456ea6c4f857b1dea9dc6a9a912d194
Ahmed Mujtaba Chang <mujtabachang@gmail.com>
2020-12-09 14:18:13 +0100

    Basic template and functionality

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+var refSentenceValue;
+var mySentenceValue;
+var output;
 $(document).ready(function () {
     main()
 });
 
 function main() {
     console.log("Initializtion")
+
 }
+
+function buttonClick() {
+    console.log("Button clicked!")
+
+    // Get element values
+    refSentenceElement = $("#refSentence").val()
+    mySentenceValue = $("#mySentence").val()
+
+    output = $("#output")
+
+    output.html(refSentenceElement)
+
+}
+
